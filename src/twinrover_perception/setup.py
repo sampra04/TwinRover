16b37190ff8d8d192001135216ca457d7025ec8a
sampra04 <samarth.prashanth@gmail.com>
2026-02-11 14:01:45 -0500

    lots of changes

---
+++ src/twinrover_perception/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+
+package_name = 'twinrover_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share',package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*'))),
+        (os.path.join('share', package_name, 'maps'), glob(os.path.join('maps', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='me597',
+    maintainer_email='samarth.prashanth@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
